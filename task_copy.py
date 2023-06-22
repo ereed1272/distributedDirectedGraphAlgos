265e26214531679262675b7f28bbeb5b4d598fbc
Emily Reed <106772229+ereed1272@users.noreply.github.com>
2023-06-22 16:15:38 -0400

    Update task_copy.py

--- task_copy.py
+++ task_copy.py
@@ -3,18 +3,18 @@ import networkx as nx
 from process_algorithm_copy import *
 
 
-
-def strongly_connected_components(grph):
-    '''
-    Function that computes the strongly connected
-    components of a digraph and the digraph diameter.
+def finite_digraph_diameter(input_graph):
+    """
+    Function that computes the finite diameter of a directed graph.
     ------------------------------------------------
-    Input:  a networkx digraph with n nodes
-            numbered from 0 to n-1
-    Output: a list of sets [S_1,...,S_n], the set
-            S_i is the SCC where node i belongs
-    '''
-
+    Input:  a connected networkx directed graph
+    Output: integer value of the finite digraph diameter
+    """
+    
+    node_relabel = {v: i for i, v in enumerate(list(input_graph.nodes()))}
+    
+    grph = nx.DiGraph([(node_relabel[u], node_relabel[v]) for (u, v) in input_graph.edges()])
+    
     # Set initial values
     n = grph.number_of_nodes()
     x = [set([i]) for i in range(n)]
@@ -24,7 +24,7 @@ def strongly_connected_components(grph):
     k = [0]*n
 
     # compute the in-neighbors of each node
-    Nv = [list(grph.predecessors(i)) + [i] for i in range(n)]
+    nv = [list(grph.predecessors(i)) + [i] for i in range(n)]
 
     # Create a shared manager for inter-process communication
     manager = Manager()
@@ -35,28 +35,20 @@ def strongly_connected_components(grph):
     w_shared = manager.list(w)
     y_shared = manager.list(y)
     k_shared = manager.list(k)
-
     # Create a function for each process
-    
     # Main loop
     to_see = list(range(n))
-    processes = []
 
     while len(to_see) > 0:
         processes = []
 
         for node in to_see:
-            
-            # we need to give as input the x,y,z,w (non shared memory) to do the computations and store the computations'
-            # results on the shared memory x_shared,y_shared,z_shared,w_shared
-            p = Process(target=process_func, args=(node, to_see, x, y, z, w, k, Nv, x_shared, y_shared, z_shared, w_shared, k_shared))
+            p = Process(target=process_func, args=(node, to_see, x, y, z, w, k, nv[node], x_shared, y_shared, z_shared, w_shared, k_shared))
             processes.append(p)
             p.start()
 
         for p in processes:
             p.join()
-        
-
         to_see = [node for node in to_see if not w_shared[node]]
 
         # Convert shared variables back to normal lists
@@ -65,49 +57,266 @@ def strongly_connected_components(grph):
         z = list(z_shared)
         w = list(w_shared)
         y = list(y_shared)
-        
-        diam=max(k)-2
-        
+    return max(k)-2
 
-    return z, diam
+
+def strongly_connected_components(input_graph):
+    '''
+    Function that computes the strongly connected
+    components of a directed graph.
+    ------------------------------------------------
+    Input:  a connected networkx directed graph 
+    Output: a list of lists containing the nodes that 
+    form each of the strongly connected components 
+    in the directed graph
+    '''
+    
+    node_relabel = {v: i for i, v in enumerate(list(input_graph.nodes()))}
+    node_relabel_back = {i: v for (v, i) in node_relabel.items()}
+    
+    grph = nx.DiGraph([(node_relabel[u], node_relabel[v]) for (u, v) in input_graph.edges()])
+    
+    # Set initial values
+    n = grph.number_of_nodes()
+    x = [set([i]) for i in range(n)]
+    z = [set() for i in range(n)]
+    w = [False] * n
+    y = [1] * n
+
+    # compute the in-neighbors of each node
+    nv = [list(grph.predecessors(i)) + [i] for i in range(n)]
+
+    # Create a shared manager for inter-process communication
+    manager = Manager()
+
+    # Create shared variables
+    x_shared = manager.list(x)
+    z_shared = manager.list(z)
+    w_shared = manager.list(w)
+    y_shared = manager.list(y)
+
+    # Main loop
+    to_see = list(range(n))
+
+    while len(to_see) > 0:
+        processes = []
+
+        for node in to_see:
+            
+            # we need to give as input the x,y,z,w (non shared memory) to do the computations and store the computations'
+            # results on the shared memory x_shared,y_shared,z_shared,w_shared
+            p = Process(target=process_func, args=(node, to_see, x, y, z, w, None, nv[node], x_shared, y_shared, z_shared, w_shared, None))
+            processes.append(p)
+            p.start()
+
+        for p in processes:
+            p.join()
+        to_see = [node for node in to_see if not w_shared[node]]
+
+        # Convert shared variables back to normal lists
+        x = list(x_shared)
+        z = list(z_shared)
+        w = list(w_shared)
+        y = list(y_shared)
+        
+    return list(map(list, set(map(tuple, [[node_relabel_back[u] for u in el] for el in z]))))
+
+
+def source_strongly_connected_components(input_graph):
+    '''
+    Function that computes the sources strongly connected
+    components of a directed graph.
+    ------------------------------------------------
+    Input:  a networkx directed graph 
+    Output: a list of lists containing the nodes 
+    that form each of the source strongly connected components 
+    in the directed graph
+    '''
+    
+    node_relabel = {v: i for i, v in enumerate(list(input_graph.nodes()))}
+    node_relabel_back = {i: v for (v, i) in node_relabel.items()}
+    
+    grph = nx.DiGraph([(node_relabel[u], node_relabel[v]) for (u, v) in input_graph.edges()])
+    
+    # Set initial values
+    n = grph.number_of_nodes()
+    x = [set([i]) for i in range(n)]
+    z = [set() for i in range(n)]
+    w = [False] * n
+    y = [1] * n
+    flag = [(False, False)] * n
+
+    # compute the in-neighbors of each node
+    nv = [list(grph.predecessors(i)) + [i] for i in range(n)]
+
+    # Create a shared manager for inter-process communication
+    manager = Manager()
+
+    # Create shared variables
+    x_shared = manager.list(x)
+    z_shared = manager.list(z)
+    w_shared = manager.list(w)
+    y_shared = manager.list(y)
+    flag_shared = manager.list(flag)
+    
+    # Main loop
+    to_see = list(range(n))
+
+    while len(to_see) > 0:
+        processes = []
+
+        for node in to_see:
+            
+            # we need to give as input the x,y,z,w (non shared memory) to do the computations and store the computations'
+            # results on the shared memory x_shared,y_shared,z_shared,w_shared
+            p = Process(target=process_func, args=(node, to_see, x, y, z, w, None, nv[node], x_shared, y_shared, z_shared, w_shared, None, flag, flag_shared, 1))
+            processes.append(p)
+            p.start()
+
+        for p in processes:
+            p.join()
+        to_see = [node for node in to_see if not w_shared[node]]
+
+        # Convert shared variables back to normal lists
+        x = list(x_shared)
+        z = list(z_shared)
+        w = list(w_shared)
+        y = list(y_shared) 
+        flag = list(flag_shared)
+    
+    return list(map(list, set(map(tuple, [[(node_relabel_back[u]) for u in el] for i, el in enumerate(z) if flag[i][1]]))))
+
+
+def target_strongly_connected_components(input_graph):
+    '''
+    Function that computes the target strongly connected
+    components of a directed graph.
+    ------------------------------------------------
+    Input:  a networkx directed graph 
+    Output: a list of lists containing the nodes 
+    that form each of the target strongly connected components
+    in the directed graph
+    '''
+    
+    node_relabel = {v: i for i, v in enumerate(list(input_graph.nodes()))}
+    node_relabel_back = {i: v for (v, i) in node_relabel.items()}
+    
+    grph = nx.DiGraph([(node_relabel[u], node_relabel[v]) for (u, v) in input_graph.edges()])
+    
+    # Set initial values
+    n = grph.number_of_nodes()
+    x = [set([i]) for i in range(n)]
+    z = [set() for i in range(n)]
+    w = [False] * n
+    y = [1] * n
+    flag = [(False, False)] * n
+
+    # compute the in-neighbors of each node
+    nv = [list(grph.predecessors(i)) + [i] for i in range(n)]
+    nvout = [list(grph.successors(i)) + [i] for i in range(n)]
+
+    # Create a shared manager for inter-process communication
+    manager = Manager()
+
+    # Create shared variables
+    x_shared = manager.list(x)
+    z_shared = manager.list(z)
+    w_shared = manager.list(w)
+    y_shared = manager.list(y)
+    flag_shared = manager.list(flag)
+
+    # Main loop
+    to_see = list(range(n))
+
+    while len(to_see) > 0:
+        processes = []
+
+        for node in to_see:
+            
+            # we need to give as input the x,y,z,w (non shared memory) to do the computations and store the computations'
+            # results on the shared memory x_shared,y_shared,z_shared,w_shared
+            p = Process(target=process_func, args=(node, to_see, x, y, z, w, None, nv[node], x_shared, y_shared, z_shared, w_shared, None, flag, flag_shared, 2, nvout[node]))
+            processes.append(p)
+            p.start()
+
+        for p in processes:
+            p.join()
+        to_see = [node for node in to_see if not w_shared[node]]
+
+        # Convert shared variables back to normal lists
+        x = list(x_shared)
+        z = list(z_shared)
+        w = list(w_shared)
+        y = list(y_shared) 
+        flag = list(flag_shared)
+
+    return list(map(list, set(map(tuple, [[node_relabel_back[u] for u in el] for i, el in enumerate(z) if flag[i][1]]))))
+
 
 def main():
-    # Create a directed graph
-    edges = [(x-1,y-1) for x,y in [(1, 2), (2, 1), (2, 3), (3, 4), (4, 3), (4, 5), (5, 6), (6, 5)]]
+    edges = [(x, y) for x, y in [(1, 2), (2, 1), (2, 3), (3, 4), (4, 3), (4, 5), (5, 6), (6, 5)]]
+    #names = {1:'mary', 2:'joseph',3:'will',4:'sarah',5:'tom',6:'alex'}
+    #edges = [(names[i],names[j]) for i,j, in [(1, 2), (2, 1), (2, 3), (3, 4), (4, 3), (4, 5), (5, 6), (6, 5)]]
     G1 = nx.DiGraph()
     G1.add_edges_from(edges)
 
-    # Compute strongly connected components
-    sccs, diam = strongly_connected_components(G1)
+    sccs = strongly_connected_components(G1)
+    diam = finite_digraph_diameter(G1)
+    source = source_strongly_connected_components(G1)
+    target = target_strongly_connected_components(G1)
 
-    # Print the results
     print("Strongly Connected Components of G1:")
     print(sccs)
     print("Diameter of G1:", diam)
+    print("Source:", source)
+    print("Target:", target)
     
-    edges = [(i,j) for i in range(5) for j in range(5) if i != j ]
+    edges = [(i, j) for i in range(5) for j in range(5) if i != j]
     G2 = nx.DiGraph()
     G2.add_edges_from(edges)
     
-    # Compute strongly connected components
-    sccs, diam = strongly_connected_components(G2)
-
-    # Print the results
+    sccs = strongly_connected_components(G2)
+    diam = finite_digraph_diameter(G2)
+    source = source_strongly_connected_components(G2)
+    target = target_strongly_connected_components(G2)
+    
     print("Strongly Connected Components of G2:")
     print(sccs)
     print("Diameter of G2:", diam)
+    print("Source:", source)
+    print("Target:", target)
     
-    edges = [(i-1,j-1) for i,j in [(1,2),(1,3),(2,4),(2,5),(2,6),(4,8),(4,9),(3,7)]]
+    edges = [(i-1, j-1) for i, j in [(1, 2), (1, 3), (2, 4), (2, 5), (2, 6), (4, 8), (4, 9), (3, 7)]]
     G3 = nx.DiGraph()
     G3.add_edges_from(edges)
     
-    # Compute strongly connected components
-    sccs, diam = strongly_connected_components(G3)
-
-    # Print the results
+    sccs = strongly_connected_components(G3)
+    diam = finite_digraph_diameter(G3)
+    source = source_strongly_connected_components(G3)
+    target = target_strongly_connected_components(G3)
+    
     print("Strongly Connected Components of G3:")
     print(sccs)
     print("Diameter of G3:", diam)
+    print("Source:", source)
+    print("Target:", target)
+    
+    edges = [[1, 4], [2, 6], [4, 1], [4, 7], [4, 8], [5, 9], [6, 5], [6, 8], [6, 9], [6, 10], [7, 3], [8, 5], [9, 3]]
+    G4 = nx.DiGraph()
+    G4.add_edges_from(edges)
+    
+    # Compute strongly connected components
+    sccs = strongly_connected_components(G4)
+    diam = finite_digraph_diameter(G4)
+    source = source_strongly_connected_components(G4)
+    target = target_strongly_connected_components(G4)
+    
+    print("Strongly Connected Components of G4:")
+    print(sccs)
+    print("Diameter of G4:", diam)
+    print("Sources: ", source_strongly_connected_components(G4))
+    print("Targets: ", target_strongly_connected_components(G4))
+
 
 if __name__ == '__main__':
     main()
